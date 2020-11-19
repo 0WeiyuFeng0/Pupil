461f93a1f4a9a047a55f58aa9fb1dab24bab8c82
mahmoudthabit <54415304+mahmoudthabit@users.noreply.github.com>
2020-11-19 17:04:15 -0600

    patient list update
    patient list can get patient emails from database

--- pupil/src/app/pages/patient-list/patient-list.component.ts
+++ pupil/src/app/pages/patient-list/patient-list.component.ts
@@ -1,3 +1,6 @@
+import { Observable } from 'rxjs';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { DataHandlerService } from './../../services/data-handler.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientListComponent implements OnInit {
 
-  constructor() { }
+  itemList: Observable<any>;
+  isDoctor: Boolean = false;
+  itemsRef: AngularFireList<any>;
+  userKey: String = null;
+
+  constructor(public dataHandler: DataHandlerService, private db: AngularFireDatabase) {
+    this.itemList = dataHandler.getData().snapshotChanges();
+   }
 
   ngOnInit(): void {
+    this.isDoctor = this.dataHandler.isDoctor();
+    this.userKey = this.dataHandler.getUserKey();
+    this.itemsRef = this.db.list(String('credentials/'+this.userKey+'/patientEmail'));
+    this.itemList = this.itemsRef.snapshotChanges();
+    this.getEmail();
+  }
+
+  getEmail(){
+    this.itemList.subscribe(item =>{
+      item.forEach(element => {
+        console.log(element.payload.val().patientEmail);
+      });
+    });
   }
 
 }
