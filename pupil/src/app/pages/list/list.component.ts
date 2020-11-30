e2583e8030d0d92090b365d99715431feb91a2e6
mahmoudthabit <54415304+mahmoudthabit@users.noreply.github.com>
2020-11-30 16:32:49 -0600

    Update list.component.ts

--- pupil/src/app/pages/list/list.component.ts
+++ pupil/src/app/pages/list/list.component.ts
@@ -1,3 +1,7 @@
+import { DataHandlerService } from './../../services/data-handler.service';
+import { element } from 'protractor';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  id: String = "1";
+  name: String = "";
+  type: String = "";
+  dateAndTime: String = "";
+  treatment: String = "";
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
+  
+  itemList: Observable<any>;
+  isDoctor: Boolean = false;
+  itemsEmailRef: AngularFireList<any>;
+  userKey: String = null;
+
+
+
+  constructor(private db: AngularFireDatabase, private dataHandler: DataHandlerService, private db2: AngularFireDatabase) {
+    this.itemList = dataHandler.getData().snapshotChanges();
+  }
 
   ngOnInit(): void {
+    this.isDoctor = this.dataHandler.isDoctor();
+    this.userKey = this.dataHandler.getUserKey();
+    this.itemsEmailRef = this.db2.list(String('credentials/'+this.userKey+'/patientEmail'));
+    this.itemList = this.itemsEmailRef.snapshotChanges();
+    this.getEmail();
+
+    this.itemsRef = this.db.list('test');
+    this.getTest();
+  }
+
+  getTest(){
+    this.items = this.itemsRef.valueChanges();
+    this.items.forEach(function(item){
+      item.forEach(function(element) {
+        // console.log(typeof element);     
+        // console.log(element);  
+        var test = element[Object.keys(element)[0]];
+        console.log(test.email);   
+      });
+      // console.log(typeof item);
+      // console.log(item);
+      // var test = item[Object.keys(item)[0]];
+      // console.log(test);
+      // console.log(test[Object.keys(test)[0]].email);
+    });
+      
+  }
+
+  getEmail(){
+    this.itemList.subscribe(item =>{
+      item.forEach(element => {
+        console.log(element.payload.val().patientEmail);
+      });
+    });
   }
 
 }
