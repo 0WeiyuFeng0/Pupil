8b2b7f10b2ce40d55d6256e92ae03bc02b347ebf
Weiyu Feng <47650296+0WeiyuFeng0@users.noreply.github.com>
2020-12-08 00:38:11 -0600

    visualization fixed

--- pupil/src/assets/js/mytest.js
+++ pupil/src/assets/js/mytest.js
@@ -65,13 +65,30 @@ function myTest() {
 target_position = [];
 left_gaze_intersection = [];
 right_gaze_intersection = [];
+error_distance = [];
+left_diameter = [];
+right_diameter = [];
+timestamp = [];
+
 
 function initialPython(test_data)
 {
+  this.target_position = [];
+  this.left_gaze_intersection = [];
+  this.right_gaze_intersection = [];
+  this.error_distance = [];
+  this.left_diameter = [];
+  this.right_diameter = [];
+  this.timestamp = [];
+
   test_data.forEach(element => {
     this.target_position.push(element.target_position);
     this.left_gaze_intersection.push(element.left_gaze_intersection);
     this.right_gaze_intersection.push(element.right_gaze_intersection);
+    this.error_distance.push(element.error_distance);
+    this.left_diameter.push(element.left_diameter);
+    this.right_diameter.push(element.right_diameter);
+    this.timestamp.push(element.timestamp);
   });
   
   //console.log(test_data);
@@ -107,6 +124,7 @@ function initialPython(test_data)
         // print("hi: " + data)
         // `);
 
+        // gaze info
         pyodide.runPython(`
         import io, base64
         import matplotlib.pyplot as plt
@@ -116,7 +134,7 @@ function initialPython(test_data)
         left_gaze_intersection = js.left_gaze_intersection
         right_gaze_intersection = js.right_gaze_intersection
         length = len(target_position)
-
+      
 
         x1 = []
         y1 = []
@@ -149,6 +167,7 @@ function initialPython(test_data)
 
         fig, (ax1) = plt.subplots(1,figsize=(6,6))
 
+        ax1.set_title('gaze')
         ax1.plot(x1, y1, label = 'target')
         ax1.plot(x2, y2, label = 'left gaze')
         ax1.plot(x3, y3, label = 'right gaze')
@@ -160,11 +179,77 @@ function initialPython(test_data)
         fig.savefig(buf, format='png')
         buf.seek(0)
         img_str = 'data:image/png;base64,' + base64.b64encode(buf.read()).decode('UTF-8')
-        buf.flush()
         `);
 
-
         document.getElementById("pyplotfigure2").src=pyodide.globals.img_str;
+      
+        // error distance
+        pyodide.runPython(`
+        import io, base64
+        import matplotlib.pyplot as plt
+        import js
+
+        error_distance = js.error_distance
+        timestamp = js.timestamp
+        length = len(timestamp)
+            
+        x4 = []
+        y4 = []
+        for i in range(0,length-1):
+          if(float(timestamp[i]-timestamp[0])*0.001 > 0):
+            x4.append(float(timestamp[i]-timestamp[0])*0.001)
+            y4.append(float(error_distance[i]))
+
+        fig, (ax1) = plt.subplots(1,figsize=(6,6))
+
+        ax1.set_title('Error')
+
+        ax1.plot(x4, y4)
+
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png')
+        buf.seek(0)
+        img_str = 'data:image/png;base64,' + base64.b64encode(buf.read()).decode('UTF-8')
+        `);
+        document.getElementById("pyplotfigure3").src=pyodide.globals.img_str;
+      
+        // error distance degree
+        pyodide.runPython(`
+        import io, base64
+        import matplotlib.pyplot as plt
+        import js
+
+        left_diameter = js.left_diameter
+        right_diameter = js.right_diameter 
+        timestamp = js.timestamp
+        length = len(timestamp)
+            
+        x4 = []
+        y4 = []
+        y5 = []
+    
+        for i in range(0,length-1):
+          if(float(timestamp[i]-timestamp[0])*0.001 > 0):
+            x4.append(float(timestamp[i]-timestamp[0])*0.001)
+            y4.append(float(left_diameter[i]))
+            y5.append(float(right_diameter[i]))
+
+
+        fig, (ax1) = plt.subplots(1,figsize=(6,6))
+
+        ax1.set_title('Diameter')
+
+        ax1.plot(x4, y4)
+        ax1.plot(x4, y5)
+
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png')
+        buf.seek(0)
+        img_str = 'data:image/png;base64,' + base64.b64encode(buf.read()).decode('UTF-8')
+        `);
+        document.getElementById("pyplotfigure4").src=pyodide.globals.img_str;
+      
+      
       });
     });
 
